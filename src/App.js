30c0981fda037605b5580ca5639b793cf82e2f98
HassenH1 <hasansaid51@gmail.com>
2020-09-20 15:02:02 -0700

    ...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Header from "./components/header";
-import './App.scss'
+import "./App.scss";
+import Headline from "./components/headline";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <section className="main">
+        <Headline
+          header="Posts"
+          description="click the button to render posts"
+        />
+      </section>
     </div>
   );
 }
---
+++ src/components/headline/headline.test.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Headline from "./index";
+import { findByTestAttr } from "./../../Utils/index";
+
+const setup = (props = {}) => {
+  const component = shallow(<Headline {...props} />);
+  return component;
+};
+
+describe("Headline Component", () => {
+  describe("have props", () => {
+    let wrapper;
+    beforeEach(() => {
+      const props = {
+        header: "test header",
+        description: "some test description",
+      };
+      wrapper = setup(props);
+    });
+
+    it("should render without errors", () => {
+      const component = findByTestAttr(wrapper, "HeadlineComponent");
+      expect(component.length).toBe(1);
+    });
+
+    it("should render a H1", () => {
+      const h1 = findByTestAttr(wrapper, "header");
+      expect(h1.length).toBe(1);
+    });
+  });
+
+  describe("have no props", () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = setup();
+    });
+  });
+});
